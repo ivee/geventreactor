7a5f8a903a2d7fdc5f39180e4be8fba4886e2c1f
Jim Cortez <jim@jimcortez.com>
2015-12-09 16:46:22 -0800

    Update to version 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'geventreactor',
-	version = '0.1.0',
+	version = '0.1.1',
 	description = 'Twisted reactor based on gevent',
 	long_description = 'geventreactor is a gevent-powered Twisted reactor whose goal is to enable mixing of gevent- and Twisted-oriented code, allowing developers to benefit from the performance of libevent and greenlet while retaining access to the extensive functionality of Twisted.',
 	license = 'MIT',
